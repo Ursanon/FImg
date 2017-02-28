80362a50d4d7057d78b04494fe8eb22766ccd11b
Ursanon <krzysztof.begiedza@gmail.com>
2017-02-28 16:57:50 +0100

    Refactoring - memory optimalization

--- src/main.cpp
+++ src/main.cpp
@@ -11,40 +11,39 @@ const size_t BPP_OUT = 1;
 const size_t COLOR_MEM = HEIGHT * WIDTH * BPP_COLOR;
 const size_t GRAY_MEM = HEIGHT * WIDTH * BPP_OUT; 
 
-uint8_t lena [COLOR_MEM];
+uint8_t lena [BPP_COLOR];
 
 //output:
-//grayscale 8BPP
-uint8_t output [GRAY_MEM];
+//grayscale 1BPP
+uint8_t output [BPP_OUT];
 
-
-
-void loadImage();
 uint8_t GetLuminosityFromRGB(uint8_t r, uint8_t g, uint8_t b);
 void convertToGray();
 void save();
 
-int main()
+int main(int argc, char * argv[])
 {
-    loadImage();
-
     convertToGray();
     
     save();
 }
 
-void loadImage()
+void convertToGray()
 {
     FILE* f = fopen("lena.raw","rb");
     if(f != NULL)
     {
-        fread(lena,COLOR_MEM,1,f);
+        for(int i = 0, j = 0; i < GRAY_MEM;++i)
+        {
+            fread(lena,BPP_COLOR,1,f);
+            output[i] = GetLuminosityFromRGB(lena[j],lena[j+1],lena[j+2]);
+        }
+        save();
+
         fclose(f);
     }
     else
-    {
         puts("ERROR with file open!");
-    }
 }
 
 uint8_t GetLuminosityFromRGB(uint8_t r, uint8_t g, uint8_t b)
@@ -52,15 +51,14 @@ uint8_t GetLuminosityFromRGB(uint8_t r, uint8_t g, uint8_t b)
     return (0.2126*r + 0.7152*g + 0.0722*b);
 }
 
-void convertToGray()
-{
-    for(int i = 0, j = 0; i < GRAY_MEM;++i,j+=3)
-        output[i] = GetLuminosityFromRGB(lena[j],lena[j+1],lena[j+2]);
-}
-
 void save()
 {
     FILE * f = fopen("out.raw","wb");
-    fwrite(output,GRAY_MEM,1,f);
-    fclose(f);
+    if(f != NULL)
+    {  
+        fwrite(output,GRAY_MEM,1,f);
+        fclose(f);
+    }
+    else
+        puts("ERROR with file open!");
 }
