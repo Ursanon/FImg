e5dcf510979512c697161db9cd5fd0edffb2aaee
Krzysztof <krzysztof.begiedza@gmail.com>
2017-03-03 14:51:46 +0100

    Reading(raw) PNG signature and image size

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include <cstdio>
+#include <cstdint>
+
+
+const int HEIGHT_CHUNK = 4;
+const int WIDTH_CHUNK = 4;
+
+int main()
+{
+
+    unsigned int uint32_dr [1];
+    uint8_t unit8_dr [1];
+
+    FILE * f = fopen("lena.png","rb");
+    if(f != NULL)
+    {
+////////////////////
+//PNG signature
+      
+      
+        for(int i = 0; i < 8;i++)
+        {
+            //todo: delete magic numbers
+            if(fread(unit8_dr,sizeof(uint8_t),1,f) != 1)
+                printf("error!\n");
+        printf("0x%X\n",*unit8_dr);
+                
+        }
+//end of PNG signature
+////////////////////
+
+        puts("___\nChunks\n___");
+
+////////////////////
+//IHDR chunk
+        ////////////////////
+        //chunk length        
+        if(fread(uint32_dr,sizeof(unsigned int),1,f) < 0)
+            printf("error!\n");    
+        printf("--\nLEN:0x%X\n",*uint32_dr);
+        
+        ////////////////////
+        //chunk type-code
+        if(fread(uint32_dr,sizeof(unsigned int),1,f) < 0)
+            printf("error!\n");
+        printf("--\nType:0x%X\n",*uint32_dr);
+
+        ////////////////////
+        //img width
+        if(fread(uint32_dr,sizeof(unsigned int),1,f) < 0)
+            printf("error!\n");
+        printf("--\nWidth:0x%X\n",*uint32_dr);
+
+        ////////////////////
+        //img height
+        if(fread(uint32_dr,sizeof(unsigned int),1,f) < 0)
+            printf("error!\n");
+        printf("--\nWidth:0x%X\n",*uint32_dr);
+
+//end of IHDR chunk
+////////////////////
+
+
+        fclose(f);
+    }
+
+}
