16d23d1834b362054c3174dfdb8f9cfada221215
Ursanon <krzysztof.begiedza@gmail.com>
2017-03-05 16:42:59 +0100

    added byteswap for uint32, png size analysis

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include <cstdio>
 #include <cstdint>
 
-
 const int HEIGHT_CHUNK = 4;
 const int WIDTH_CHUNK = 4;
 
+void byteswap_uint32(unsigned int &x)
+{
+    x = (x>>24) | 
+    ((x<<8) & 0x00FF0000) |
+    ((x>>8) & 0x0000FF00) |
+    (x<<24);
+}
+
+struct IMG_SIZE {
+    uint32_t width;
+    uint32_t height;    
+};
+
 int main()
 {
-
+    IMG_SIZE image;
     unsigned int uint32_dr [1];
     uint8_t uint8_dr [1];
 
@@ -23,7 +35,7 @@ int main()
             //todo: delete magic numbers
             if(fread(uint8_dr,sizeof(uint8_t),1,f) != 1)
                 printf("error!\n");
-        printf("0x%X\n",*uint8_dr);
+            printf("%u\n",*uint8_dr);
                 
         }
 //end of PNG signature
@@ -37,27 +49,39 @@ int main()
         //chunk length        
         if(fread(uint32_dr,sizeof(unsigned int),1,f) < 0)
             printf("error!\n");    
-        printf("--\nLEN:0x%X\n",*uint32_dr);
+        printf("--\nLEN:%u\n",*uint32_dr);
         
         ////////////////////
         //chunk type-code
         if(fread(uint32_dr,sizeof(unsigned int),1,f) < 0)
             printf("error!\n");
-        printf("--\nType:0x%X\n",*uint32_dr);
+        printf("--\nType:0x%X\n--\n",*uint32_dr);
 
         ////////////////////
         //img width
-        if(fread(uint32_dr,sizeof(unsigned int),1,f) < 0)
-            printf("error!\n");
-        printf("--\nWidth:0x%X\n",*uint32_dr);
+        fread(&image.width,sizeof(unsigned int),1,f);
+        byteswap_uint32(image.width);
+
+        printf("Width:%u\n",image.width);
 
         ////////////////////
 
+        ////////////////////
+        //img height
+        fread(&image.height,sizeof(unsigned int),1,f);
+        byteswap_uint32(image.height);
+
+        printf("Height:%u\n",image.height);
+        ////////////////////
+
+
+
+/*
         uint8_t uint8_dra[4];
         fread(uint8_dra,sizeof(uint8_t),4,f);
         for(int i = 3 ;i>-1;--i)
             printf("%d ",*(uint8_dra+i));
-
+*/
 /*
         //img height
         if(fread(uint8_dr,sizeof(uint8_t),1,f) < 0)
