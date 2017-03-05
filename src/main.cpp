d558052d3b72e573144c51d4a19b291bb545dafb
Krzysztof <krzysztof.begiedza@gmail.com>
2017-03-05 10:03:54 +0100

    reading byte after byte PNG file format

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main()
 {
 
     unsigned int uint32_dr [1];
-    uint8_t unit8_dr [1];
+    uint8_t uint8_dr [1];
 
     FILE * f = fopen("lena.png","rb");
     if(f != NULL)
@@ -21,9 +21,9 @@ int main()
         for(int i = 0; i < 8;i++)
         {
             //todo: delete magic numbers
-            if(fread(unit8_dr,sizeof(uint8_t),1,f) != 1)
+            if(fread(uint8_dr,sizeof(uint8_t),1,f) != 1)
                 printf("error!\n");
-        printf("0x%X\n",*unit8_dr);
+        printf("0x%X\n",*uint8_dr);
                 
         }
 //end of PNG signature
@@ -52,11 +52,34 @@ int main()
         printf("--\nWidth:0x%X\n",*uint32_dr);
 
         ////////////////////
-        //img height
-        if(fread(uint32_dr,sizeof(unsigned int),1,f) < 0)
-            printf("error!\n");
-        printf("--\nWidth:0x%X\n",*uint32_dr);
 
+        uint8_t uint8_dra[4];
+        fread(uint8_dra,sizeof(uint8_t),4,f);
+        for(int i = 3 ;i>-1;--i)
+            printf("%d ",*(uint8_dra+i));
+
+/*
+        //img height
+        if(fread(uint8_dr,sizeof(uint8_t),1,f) < 0)
+            printf("error!\n");
+        printf("--\nWidth:0x%X\n",*uint8_dr);
+        
+        //img height
+        if(fread(uint8_dr,sizeof(uint8_t),1,f) < 0)
+            printf("error!\n");
+        printf("--\nWidth:0x%X\n",*uint8_dr);
+
+        //img height
+        if(fread(uint8_dr,sizeof(uint8_t),1,f) < 0)
+            printf("error!\n");
+        printf("--\nWidth:0x%X\n",*uint8_dr);
+
+        //img height
+        if(fread(uint8_dr,sizeof(uint8_t),1,f) < 0)
+            printf("error!\n");
+        printf("--\nWidth:0x%X\n",*uint8_dr);
+*/
+        
 //end of IHDR chunk
 ////////////////////
 
